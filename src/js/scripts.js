a9c9e220a874684afb56d2b1e3cac467ab862e1e
Gregori Orlov <orlov.gregori@yandex.ru>
2023-12-05 19:47:16 +0300

    сверстать главную, сделать адаптив

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,3 +21,23 @@ const promoSlider = new Swiper ('.first__slider-promo', {
     el: '.swiper-pagination',
   },
 })
+
+const promoCodesSlider = new Swiper ('.promocodes__block', {
+   autoplay: {
+      delay: 3000,
+   },
+   speed: 700,
+   slidesPerView: 1.2,
+   spaceBetween: 10,
+   breakpoints: {
+      500: {
+         slidesPerView: 2,
+         centeredSlides: true,
+      },
+      834: {
+         slidesPerView: 3,
+      }
+   },
+
+   loop: true,
+  })
